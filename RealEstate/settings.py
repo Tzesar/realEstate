00cdb703def16c9594f697230a6f0ef3d0aee457
Augusto Amarilla <augusto@elumen.info>
2018-04-07 15:31:01 -0400

    Update Django-bootstrap to version 4 from 3

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RequestsApp.apps.RequestsAppConfig',
+    'bootstrap4',
+    'RequestsApp.apps.RequestsAppConfig'
 ]
 
 MIDDLEWARE = [
