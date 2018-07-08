6acbff43a6e17f33051682d81e47bd90d34590a4
Augusto Amarilla <agu.amarilla@gmail.com>
2018-07-08 13:29:38 -0400

    Updated the deployment script

--- updateProject.sh
+++ updateProject.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-git fetch --all
-git checkout development
-git pull origin development
+echo "Updating project data from repository: "
 
-python manage.py collectstatic
+LOGFILE="deploy.log"
+DEBUGLOG=/dev/null
+
+if [ "$1" = "--verbose" ]; then
+	DEBUGLOG=$LOGFILE
+	echo "Verbose mode enabled"
+fi
+
+echo "Retrieving latest changes from repo:" $(git remote get-url --all origin)
+git fetch --all >> $DEBUGLOG 2>> $LOGFILE
+git checkout master >> $DEBUGLOG 2>> $LOGFILE
+git pull origin master >> $DEBUGLOG 2>> $LOGFILE
+
+python manage.py migrate >> $DEBUGLOG 2>> $LOGFILE
+python manage.py collectstatic --clear --no-input >> $DEBUGLOG 2>> $LOGFILE
